7e1bca6d98e44cde4e01f2674a0d1aefb4edf82e
Sayan <thediscoverypost@gmail.com>
2024-09-08 23:24:18 +0530

    Add 200-day moving average calculation to data analysis script

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt
+import  matplotlib.pyplot as plt
 
 start = '2014-01-01'
 end = '2024-08-30'
@@ -20,3 +20,5 @@ plt.figure(figsize=(8,6))
 plt.plot(ma_100_days, 'r')
 plt.plot(data.Close, 'g')
 plt.show()
+
+ma_200_days = data.Close.rolling(200).mean()
